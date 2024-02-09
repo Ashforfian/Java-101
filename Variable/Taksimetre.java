fc0b573de9c6e13372b7323faba9eda5259999bc
Ashforfian <y.keemsahan2626@gmail.com>
2024-02-10 02:21:08 +0300

    Taksimetre

--- Variable/Taksimetre.java
+++ Variable/Taksimetre.java
@@ -5,12 +5,13 @@ import java.util.Scanner;
 
 public class Taksimetre {
     public static void main(String[] args) {
+        double PRICE_PER_KM = 2.20, START_PRICE = 10, MIN_PRICE = 20;
         Scanner input = new Scanner(System.in);
         input.useLocale(Locale.US);
         System.out.print("Gidilen mesafeyi giriniz(KM): ");
         double km = input.nextDouble();
-        double taxiAmount = 10 + km * 2.20;
-        double price = taxiAmount>20 ? taxiAmount : 20;
+        double taxiAmount = START_PRICE + km * PRICE_PER_KM;
+        double price = taxiAmount>MIN_PRICE ? taxiAmount : MIN_PRICE;
         System.out.println("Taksi tutarı: "+price+"TL");
     }
 }
