2d7ca245063a507956fde009fbb593e5744800ce
Ashforfian <y.keemsahan2626@gmail.com>
2024-02-26 20:17:14 +0300

    Polindrom

---
+++ 7-Others/Polindrom.java
@@ -0,0 +1,17 @@
+package Others;
+
+public class Polindrom {
+    static boolean isPolindrom(String str) {
+        for (int i = 0, j = str.length()-1; i < j; i++,j--){
+            if (str.charAt(i) != str.charAt(j)){
+                return false;
+            }
+        }
+        return true;
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPolindrom("abba"));
+    }
+}
